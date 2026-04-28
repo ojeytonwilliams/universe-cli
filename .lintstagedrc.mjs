954b55276c217662b3c53d71ed12f42e0ef4cc7d
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-04-28 14:06:52 +0200

    fix: lint-staged relative to cwd

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,10 +1,17 @@
+import path from "node:path";
 import picomatch from "picomatch";
+
+const removeFilesFolder = (files) => {
+  const cwd = process.cwd();
+  const relativePaths = files.map((file) => path.relative(cwd, file));
+  const matcher = picomatch(["files/**"]);
+  return relativePaths.filter((f) => !matcher(f));
+};
 
 // oxlint-disable-next-line import/no-default-export import/no-anonymous-default-export
 export default {
   "*.{js,jsx,ts,tsx,mjs,cjs}": (files) => {
-    // Filter out ignored files
-    const match = files.filter((f) => picomatch.isMatch(f, "files/**"));
+    const match = removeFilesFolder(files);
     if (match.length === 0) return [];
     const matchingFiles = match.join(" ");
 
@@ -13,7 +20,7 @@ export default {
 
   "!*.{js,jsx,ts,tsx,mjs,cjs}": (files) => {
     // Filter out ignored files
-    const match = files.filter((f) => picomatch.isMatch(f, "files/**"));
+    const match = removeFilesFolder(files);
     if (match.length === 0) return [];
     const matchingFiles = match.join(" ");
 
